fadef4dc52325d176be524bef0037b8ffbf90d92
clintatmo <Cl1nt@github>
2017-02-14 15:18:37 -0300

    Vuetable component

--- src/components/settings/modules/SettingsModules.vue
+++ src/components/settings/modules/SettingsModules.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="settings-admin">
+  <div id="settings-modules">
     <div class="panel panel-default">
       <div class="panel-heading">
         <h3 class="panel-title">Modules</h3>
       </div>
       <div class="panel-body">
-        <h1>MODULES</h1>
+        <settings-modules-vuetable></settings-modules-vuetable>
       </div>
     </div>
 
@@ -14,7 +14,11 @@
 
 <script>
 
+  import Vue from 'vue'
   import Sidebar from 'components/dashboard/Sidebar.vue';
+  import SettingsModulesVuetable from 'components/settings/modules/SettingsModulesVuetable'
+
+  Vue.component('settings-modules-vuetable', SettingsModulesVuetable)
 
   export default {
     name: 'settings-modules',
@@ -29,7 +33,10 @@
   }
 </script>
 
-<style scoped>
+<style>
 
+  table.vuetable {
+    text-align: left;
+  }
 
 </style>
---
+++ src/components/settings/modules/SettingsModulesVuetable.vue
@@ -0,0 +1,193 @@
+<template>
+  <div>
+    <filter-bar></filter-bar>
+    <vuetable ref="vuetable"
+      api-url="http://localhost:4500/api/module"
+      :fields="fields"
+      :css="css"
+      pagination-path=""
+      :per-page="20"
+      :multi-sort="true"
+      multi-sort-key="ctrl"
+      :sort-order="sortOrder"
+      detail-row-component="my-detail-row"
+      :append-params="moreParams"
+      @vuetable:cell-clicked="onCellClicked"
+      @vuetable:pagination-data="onPaginationData">
+    </vuetable>
+    <div>
+      <vuetable-pagination-info ref="paginationInfo"
+        info-class="pull-left">
+      </vuetable-pagination-info>
+      <vuetable-pagination ref="pagination"
+        :css="cssPagination"
+        :icons="icons"
+        @vuetable-pagination:change-page="onChangePage">
+      </vuetable-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import Vuetable from '../../vue-table/Vuetable'
+import VuetablePagination from '../../vue-table/VuetablePagination'
+import VuetablePaginationInfo from '../../vue-table/VuetablePaginationInfo'
+import CustomActions from '../../vue-table/CustomActions'
+import DetailRow from '../../vue-table/DetailRow'
+import FilterBar from '../../vue-table/FilterBar'
+import VueEvents from 'vue-events'
+
+Vue.use(VueEvents)
+
+Vue.component('custom-actions', CustomActions)
+Vue.component('my-detail-row', DetailRow)
+
+export default {
+  components: {
+    Vuetable: Vuetable,
+    VuetablePagination: VuetablePagination,
+    VuetablePaginationInfo: VuetablePaginationInfo,
+    FilterBar: FilterBar
+  },
+  data () {
+  	return {
+      css: {
+        tableClass: 'table table-striped table-bordered',
+        loadingClass: 'loading',
+        ascendingIcon: 'glyphicon glyphicon-chevron-up',
+        descendingIcon: 'glyphicon glyphicon-chevron-down',
+        sortHandleIcon: 'glyphicon glyphicon-menu-hamburger',
+      },
+      cssPagination: {
+        wrapperClass: 'pagination pull-right',
+        activeClass: 'btn-primary',
+        disabledClass: 'disabled',
+        pageClass: 'btn btn-border',
+        linkClass: 'btn btn-border',
+      },
+      icons: {
+        first: '',
+        prev: '',
+        next: '',
+        last: '',
+      },
+      fields: [
+        {
+          name: '__checkbox',
+          titleClass: 'text-center',
+          dataClass: 'text-center',
+        },
+        {
+          name: 'name',
+          sortField: 'name'
+        },
+        {
+          name: 'email',
+          sortField: 'email'
+        },
+        {
+          name: 'age',
+          sortField: 'birthdate',
+          dataClass: 'text-center'
+        },
+        {
+          name: 'birthdate',
+          sortField: 'birthdate',
+          titleClass: 'text-center',
+          dataClass: 'text-center',
+          callback: 'formatDate|DD-MM-YYYY'
+        },
+        {
+          name: 'nickname',
+          sortField: 'nickname',
+          callback: 'allcap'
+        },
+        {
+          name: 'gender',
+          sortField: 'gender',
+          titleClass: 'text-center',
+          dataClass: 'text-center',
+          callback: 'genderLabel'
+        },
+        {
+          name: 'salary',
+          sortField: 'salary',
+          titleClass: 'text-center',
+          dataClass: 'text-right',
+          callback: 'formatNumber',
+        },
+        {
+          name: '__component:custom-actions',
+          title: 'Actions',
+          titleClass: 'text-center',
+          dataClass: 'text-center'
+        }
+      ],
+      sortOrder: [
+        {
+          field: 'email',
+          sortField: 'email',
+          direction: 'asc'
+        }
+      ],
+      moreParams: {}
+  	}
+  },
+  methods: {
+    allcap (value) {
+      return value.toUpperCase()
+    },
+    genderLabel (value) {
+      return value === 'M'
+        ? '<span class="label label-warning"><span class="glyphicon glyphicon-star"></span> Male</span>'
+        : '<span class="label label-info"><span class="glyphicon glyphicon-heart"></span> Female</span>'
+    },
+    formatNumber (value) {
+      return accounting.formatNumber(value, 2)
+    },
+    formatDate (value, fmt = 'D MMM YYYY') {
+      return (value == null)
+        ? ''
+        : moment(value, 'YYYY-MM-DD').format(fmt)
+    },
+    onPaginationData (paginationData) {
+      this.$refs.pagination.setPaginationData(paginationData)
+      this.$refs.paginationInfo.setPaginationData(paginationData)
+    },
+    onChangePage (page) {
+      this.$refs.vuetable.changePage(page)
+    },
+    onCellClicked (data, field, event) {
+      console.log('cellClicked: ', field.name)
+      this.$refs.vuetable.toggleDetailRow(data.id)
+    },
+  },
+  events: {
+    'filter-set' (filterText) {
+      this.moreParams = {
+        'filter': filterText
+      }
+      Vue.nextTick( () => this.$refs.vuetable.refresh())
+    },
+    'filter-reset' () {
+      this.moreParams = {}
+      this.$refs.vuetable.refresh()
+      Vue.nextTick( () => this.$refs.vuetable.refresh())
+    }
+  }
+}
+</script>
+<style>
+.pagination {
+  margin-top: 0;
+}
+.btn.btn-border {
+  border: 1px solid;
+  margin-right: 2px;
+}
+.vuetable-pagination-info {
+  margin-top: 8px !important;
+}
+</style>
