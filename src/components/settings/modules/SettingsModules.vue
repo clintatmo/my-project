dd47364e8b2417d1f6acba3fb6fb2e9d55b38a1a
clintatmo <Cl1nt@github>
2017-02-15 15:35:41 -0300

    Removed Vuetable component

--- src/components/settings/modules/SettingsModules.vue
+++ src/components/settings/modules/SettingsModules.vue
@@ -5,7 +5,6 @@
         <h3 class="panel-title">Modules</h3>
       </div>
       <div class="panel-body">
-        <settings-modules-vuetable></settings-modules-vuetable>
       </div>
     </div>
 
@@ -16,9 +15,6 @@
 
   import Vue from 'vue'
   import Sidebar from 'components/dashboard/Sidebar.vue';
-  import SettingsModulesVuetable from 'components/settings/modules/SettingsModulesVuetable'
-
-  Vue.component('settings-modules-vuetable', SettingsModulesVuetable)
 
   export default {
     name: 'settings-modules',
